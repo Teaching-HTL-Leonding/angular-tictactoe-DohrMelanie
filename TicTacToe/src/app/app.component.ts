10fec8072ae7aa116668ba24b40f88df5427d5ab
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-09-23 10:19:28 +0200

    delete test code and add basic boxes

--- TicTacToe/src/app/app.component.ts
+++ TicTacToe/src/app/app.component.ts
@@ -1,13 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'TicTacToe';
+  currentPlayer = 'X';
+  boxes: HTMLElement[] = Array.from({ length: 9 });
+
+  onBoxClick(index: number, event: MouseEvent) {
+    console.log('Box clicked', index);
+    const clickedElement = event.target as HTMLElement; // Access the DOM element
+    clickedElement.innerText = this.currentPlayer;
+
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
 }
