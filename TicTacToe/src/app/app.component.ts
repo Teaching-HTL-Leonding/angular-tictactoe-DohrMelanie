0f1d078447135c73297367008072ad1423ee8358
DohrMelanie <m.dohr@students.htl-leonding.ac.at>
2024-09-24 22:07:58 +0200

    added tic tac toe

--- TicTacToe/src/app/app.component.ts
+++ TicTacToe/src/app/app.component.ts
@@ -10,15 +10,78 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TicTacToe';
+  title = 'Tic Tac Toe';
   currentPlayer = 'X';
-  boxes: HTMLElement[] = Array.from({ length: 9 });
+  message = "";
+  gameOver = false;
 
-  onBoxClick(index: number, event: MouseEvent) {
-    console.log('Box clicked', index);
-    const clickedElement = event.target as HTMLElement; // Access the DOM element
-    clickedElement.innerText = this.currentPlayer;
+  boxes: HTMLDivElement[][] = Array(3).fill(null).map(() =>
+    Array(3).fill(null).map(() => {
+      const div = document.createElement('div');
+      div.innerText = '';
+      return div;
+    })
+  );
 
+  onBoxClick(i: number, j: number, event: MouseEvent) {
+    if (this.gameOver) {
+      return;
+    }
+    this.boxes[i][j] = event.target as HTMLDivElement;
+    this.boxes[i][j].innerText = this.currentPlayer;
+
+    if (this.checkWin()) {
+      this.message = this.currentPlayer + ' won!';
+      this.gameOver = true;
+    }
     this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
+
+  checkWin(): boolean {
+    for (let y = 0; y < 3; y++) {
+      console.log(this.boxes[y]);
+      if (this.boxes[y] !== null && this.boxes[y].every(cell => cell && cell.innerText === this.currentPlayer)) {
+        return true;
+      }
+      console.log("after");
+    }
+
+    for (let x = 0; x < 3; x++) {
+      let columnWin = true;
+      for (let y = 0; y < 3; y++) {
+        if (this.boxes[y][x] && this.boxes[y][x].innerText !== this.currentPlayer) {
+          columnWin = false;
+          break;
+        }
+      }
+      if (columnWin) {
+        return true;
+      }
+    }
+
+    // Check main diagonal
+    let mainDiagonalWin = true;
+    for (let i = 0; i < 3; i++) {
+      if (this.boxes[i][i] && this.boxes[i][i].innerText !== this.currentPlayer) {
+        mainDiagonalWin = false;
+        break;
+      }
+    }
+    if (mainDiagonalWin) {
+      return true;
+    }
+
+    // Check anti-diagonal
+    let antiDiagonalWin = true;
+    for (let i = 0; i < 3; i++) {
+      if (this.boxes[i][3 - 1 - i] && this.boxes[i][3 - 1 - i].innerText !== this.currentPlayer) {
+        antiDiagonalWin = false;
+        break;
+      }
+    }
+    if (antiDiagonalWin) {
+      return true;
+    }
+    return false;
   }
 }
